be4474c42cc29540f748396a01e65782a95ed833
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-11-15 09:48:38 +0200

    Added new links to header.

--- app/src/components/Footer/index.jsx
+++ app/src/components/Footer/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import './styles.css';
 
-class Footer extends React.Component {
-
-    render() {
-        return <footer className="footer">
+const Footer = () => {
+    return (
+        <footer className="footer">
             <p>
                 <span>Powered by</span>
                 <a href="https://github.com/dobroslav-atanasov" target="_black"> Dobroslav Atanasov </a>
                 <span>&copy; 2019 </span>
             </p>
         </footer>
-    }
-};
+    )
+}
 
 export default Footer;
--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './styles.css';
-import Link from '../Link';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 class Header extends React.Component {
 
     render() {
         return <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="#home">Sport Data Center</Navbar.Brand>
+            <Navbar.Brand href="/">Sport Data Center</Navbar.Brand>
             <Navbar.Collapse>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                </Nav>
                 <Nav className="ml-auto">
-                    <Nav.Link href="#home">Login</Nav.Link>
-                    <Nav.Link href="#features">Register</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/register">Register</Nav.Link>
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
