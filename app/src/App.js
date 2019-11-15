7e9b2d1c4078d49d87b175780fc01ab0c37f8339
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-15 20:15:44 +0200

    Implemented routes.

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <Router>
+      <div className="App">
+        <Header />
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/about" exact component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
