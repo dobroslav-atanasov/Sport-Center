5f4e47390d12f6b68c6aa65b80c67dd003f99c46
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-19 18:39:07 +0200

    Created component - Event.

---
+++ app/src/components/Event/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Event = () => {
+    return (
+        <div className="container">
+            <br />
+            <div className="card bg-light">
+                <article className="card-body mx-auto">
+                    <h3 className="card-title mt-3 text-center">Create Event</h3>
+                    <hr />
+                    <form>
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><i className="fa fa-etsy"></i></span>
+                            </div>
+                            <input type="text" name="name" className="form-control" placeholder="Name" />
+                        </div>
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"> <i className="fa fa-calendar"></i> </span>
+                            </div>
+                            <input type="date" name="country" class="form-control" placeholder="Country" />
+                        </div>
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><i className="fa fa-globe"></i></span>
+                            </div>
+                            <select className="form-control">
+                                <option selected=""> Select Town</option>
+                                <option>Sofia</option>
+                                <option>Varna</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-warning btn-block">Create Event</button>
+                        </div>
+                    </form>
+                </article>
+            </div>
+        </div>
+    )
+};
+
+export default Event;
