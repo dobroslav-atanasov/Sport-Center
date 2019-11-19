7363ed3de528b07bb0caded25e870b4119801e79
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-11-19 08:49:00 +0200

    Updated component Home.

--- app/src/components/Home/index.jsx
+++ app/src/components/Home/index.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    const LogoImage = "Wall.jpg"
     return (
-        // <div class="jumbotron jumbotron-fluid" style={{ backgroundImage: `url(${LogoImage})`, height: 780 }}>
-        //     <div class="container">
-        //     </div>
-        // </div>
-        <div class="jumbotron">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4" />
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-        </div>
+        <Fragment>
+            <br />
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-md">
+                        <div class="card" style={{ width: 300 }}>
+                            <img src="Wall.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <Link to="/about" className="btn btn-warning btn-lg">About Us</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md">
+                        <div class="card" style={{ width: 300 }}>
+                            <img src="Wal1.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <Link to="/about" className="btn btn-warning btn-lg">About Us</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md">
+                        <div class="card" style={{ width: 300 }}>
+                            <img src="Wall.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <Link to="/about" className="btn btn-warning btn-lg">About Us</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     )
 };
 
