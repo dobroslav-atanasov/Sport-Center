70e3fad351b00d41ece2a34986661542dfae4fc1
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-24 12:28:27 +0200

    Implemented custom validation and redirect to login page.

--- app/src/components/Register/index.jsx
+++ app/src/components/Register/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 import userService from '../../services/userService';
+
+const validationForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+        (val) => val.length > 0 && (valid = false)
+    );
+
+    return valid;
+}
+
+const validationEmailRegex = RegExp(/[\w]+@[a-z]+\.com/i);
 
 class Register extends React.Component {
     constructor(props) {
@@ -14,117 +25,192 @@ class Register extends React.Component {
             lastName: '',
             email: '',
             age: '',
-            gender: ''
+            gender: '',
+            errors: {
+                username: '',
+                password: '',
+                confirmPassword: '',
+                firstName: '',
+                lastName: '',
+                email: '',
+                age: ''
+            },
+            isRedirect: false
         };
     };
 
-    submitHandler = (event) => {
+    changeHandler = (event) => {
         event.preventDefault();
+        const { name, value } = event.target;
+        let errors = this.state.errors;
+
+        switch (name) {
+            case 'username':
+                errors.username = value.length < 3 ? 'Username should be at least 3 characters long!' : '';
+                break;
+            case 'password':
+                errors.password = value.length < 3 ? 'Password should be at least 3 characters long!' : '';
+                break;
+            case 'confirmPassword':
+                errors.confirmPassword = value !== this.state.password ? 'Passwords don\'t match!' : '';
+                break;
+            case 'firstName':
+                errors.firstName = value.length === 0 ? 'First name is required!' : '';
+                break;
+            case 'lastName':
+                errors.lastName = value.length === 0 ? 'Last name is required!' : '';
+                break;
+            case 'email':
+                errors.email = !validationEmailRegex.test(value) ? 'Invalid email!' : '';
+                break;
+            case 'age':
+                errors.age = value < 16 || value > 100 ? 'Age should be between 16 and 100!' : '';
+                break;
+            default:
+                break;
+        };
+
+        this.setState({ errors, [name]: value });
+    }
+
+    submitHandler = (event) => {
         const data = this.state;
-        userService.register(data).then(() => console.log('FINISH'));
+
+        event.preventDefault();
+        if (validationForm(this.state.errors)) {
+            userService.register(data).then(() => {
+                this.setState({ isRedirect: true });
+            });
+        }
     };
 
-    usernameChangeHandler = (event) => {
-        this.setState({ username: event.target.value });
-    };
+    // usernameChangeHandler = (event) => {
+    //     this.setState({ username: event.target.value });
+    // };
 
-    passwordChangeHandler = (event) => {
-        this.setState({ password: event.target.value });
-    };
+    // passwordChangeHandler = (event) => {
+    //     this.setState({ password: event.target.value });
+    // };
 
-    confirmPasswordChangeHandler = (event) => {
-        this.setState({ confirmPassword: event.target.value });
-    };
+    // confirmPasswordChangeHandler = (event) => {
+    //     this.setState({ confirmPassword: event.target.value });
+    // };
 
-    firstNameChangeHandler = (event) => {
-        this.setState({ firstName: event.target.value });
-    };
+    // firstNameChangeHandler = (event) => {
+    //     this.setState({ firstName: event.target.value });
+    // };
 
-    lastNameChangeHandler = (event) => {
-        this.setState({ lastName: event.target.value });
-    };
+    // lastNameChangeHandler = (event) => {
+    //     this.setState({ lastName: event.target.value });
+    // };
 
-    emailChangeHandler = (event) => {
-        this.setState({ email: event.target.value });
-    };
+    // emailChangeHandler = (event) => {
+    //     this.setState({ email: event.target.value });
+    // };
 
-    ageChangeHandler = (event) => {
-        this.setState({ age: event.target.value });
-    };
+    // ageChangeHandler = (event) => {
+    //     this.setState({ age: event.target.value });
+    // };
 
-    genderChangeHandler = (event) => {
-        this.setState({ gender: event.target.value });
-    };
+    // genderChangeHandler = (event) => {
+    //     this.setState({ gender: event.target.value });
+    // };
 
     render() {
+        const { errors } = this.state;
         return (
-            <div className="container">
-                <br />
-                <div className="card bg-light">
-                    <article className="card-body mx-auto">
-                        <h3 className="card-title mt-3 text-center">Create Account</h3>
-                        <hr />
-                        <form onSubmit={this.submitHandler}>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fa fa-user"></i></span>
+            this.state.isRedirect ? <Redirect to="/login" /> :
+                <div className="container">
+                    <br />
+                    <div className="card bg-light">
+                        <article className="card-body mx-auto">
+                            <h3 className="card-title mt-3 text-center">Create Account</h3>
+                            <hr />
+                            <form onSubmit={this.submitHandler} noValidate>
+                                {/* USERNAME */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fa fa-user"></i></span>
+                                    </div>
+                                    <input type="text" className="form-control" name="username" /*value={this.state.username}*/ onChange={this.changeHandler} noValidate /*onChange={this.usernameChangeHandler}*/ placeholder="Username" />
                                 </div>
-                                <input type="text" className="form-control" value={this.state.username} onChange={this.usernameChangeHandler} placeholder="Username" />
-                            </div>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
+                                {errors.username.length > 0 && <div className="alert alert-danger">{errors.username}</div>}
+
+                                {/* PASSWORD */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
+                                    </div>
+                                    <input type="password" className="form-control" name="password" onChange={this.changeHandler} noValidate /*value={this.state.password} onChange={this.passwordChangeHandler}*/ placeholder="Password" />
                                 </div>
-                                <input type="password" className="form-control" value={this.state.password} onChange={this.passwordChangeHandler} placeholder="Password" />
-                            </div>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
+                                {errors.password.length > 0 && <div className="alert alert-danger">{errors.password}</div>}
+
+                                {/* CONFIRM PASSWORD */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
+                                    </div>
+                                    <input type="password" className="form-control" name="confirmPassword" onChange={this.changeHandler} noValidate /*value={this.state.confirmPassword} onChange={this.confirmPasswordChangeHandler}*/ placeholder="Confirm Password" />
                                 </div>
-                                <input type="password" className="form-control" value={this.state.confirmPassword} onChange={this.confirmPasswordChangeHandler} placeholder="Confirm Password" />
-                            </div>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fa fa-user"></i></span>
+                                {errors.confirmPassword.length > 0 && <div className="alert alert-danger">{errors.confirmPassword}</div>}
+
+                                {/* FIRST NAME */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fa fa-user"></i></span>
+                                    </div>
+                                    <input type="text" className="form-control" name="firstName" onChange={this.changeHandler} noValidate /*value={this.state.firstName} onChange={this.firstNameChangeHandler}*/ placeholder="First Name" />
                                 </div>
-                                <input type="text" className="form-control" value={this.state.firstName} onChange={this.firstNameChangeHandler} placeholder="First Name" />
-                            </div>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fa fa-user"></i></span>
+                                {errors.firstName.length > 0 && <div className="alert alert-danger">{errors.firstName}</div>}
+
+                                {/* LAST NAME */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fa fa-user"></i></span>
+                                    </div>
+                                    <input type="text" className="form-control" name="lastName" onChange={this.changeHandler} noValidate/*value={this.state.lastName} onChange={this.lastNameChangeHandler}*/ placeholder="Last Name" />
                                 </div>
-                                <input type="text" className="form-control" value={this.state.lastName} onChange={this.lastNameChangeHandler} placeholder="Last Name" />
-                            </div>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fa fa-envelope"></i></span>
+                                {errors.lastName.length > 0 && <div className="alert alert-danger">{errors.lastName}</div>}
+
+                                {/* EMAIL */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fa fa-envelope"></i></span>
+                                    </div>
+                                    <input type="text" className="form-control" name="email" onChange={this.changeHandler} /*value={this.state.email} onChange={this.emailChangeHandler}*/ placeholder="Email" />
                                 </div>
-                                <input type="text" className="form-control" value={this.state.email} onChange={this.emailChangeHandler} placeholder="Email" />
-                            </div>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fa fa-table"></i></span>
+                                {errors.email.length > 0 && <div className="alert alert-danger">{errors.email}</div>}
+
+                                {/* AGE */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fa fa-table"></i></span>
+                                    </div>
+                                    <input type="number" className="form-control" name="age" onChange={this.changeHandler} /*value={this.state.age} onChange={this.ageChangeHandler}*/ placeholder="Age" />
                                 </div>
-                                <input type="number" className="form-control" value={this.state.age} onChange={this.ageChangeHandler} placeholder="Age" />
-                            </div>
-                            <div className="form-group input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fa fa-male"></i></span>
+                                {errors.age.length > 0 && <div className="alert alert-danger">{errors.age}</div>}
+
+                                {/* GENDER */}
+                                <div className="form-group input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fa fa-male"></i></span>
+                                    </div>
+                                    <select className="form-control" name="gender" onChange={this.changeHandler} /*value={this.state.gender} onChange={this.genderChangeHandler}*/>
+                                        <option selected=""> Select gender</option>
+                                        <option>Male</option>
+                                        <option>Female</option>
+                                    </select>
                                 </div>
-                                <select className="form-control" value={this.state.gender} onChange={this.genderChangeHandler}>
-                                    <option selected=""> Select gender</option>
-                                    <option>Male</option>
-                                    <option>Female</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-warning btn-block">Create Account</button>
-                            </div>
-                            <p class="text-center">Have an account? <Link to="/login">Log In</Link></p>
-                        </form>
-                    </article>
+
+                                {/* SUBMIT */}
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-warning btn-block">Create Account</button>
+                                </div>
+                                <p class="text-center">Have an account? <Link to="/login">Log In</Link></p>
+                            </form>
+                        </article>
+                    </div>
                 </div>
-            </div>
         )
     }
 };
