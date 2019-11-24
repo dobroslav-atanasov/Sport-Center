46321a5557342a16ff96a570e9fbcb5fbdafdefc
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-24 10:52:08 +0200

    Modified userService.js

--- app/src/services/userService.js
+++ app/src/services/userService.js
@@ -1,7 +1,23 @@
+import axios from 'axios';
+
 const userService = {
-    load: function () {
-        return fetch(`http://localhost:3333/api/user`)
-            .then(res => res.json());
+    register: function (data) {
+        const { username, password, firstName, lastName, email, age, gender } = data;
+        return axios({
+            method: 'POST',
+            url: 'http://localhost:3333/api/user/register',
+            data: {
+                username,
+                password,
+                firstName,
+                lastName,
+                email,
+                age,
+                gender
+            }
+        }).then(res => {
+            console.log(res.data);
+        });
     }
 };
 
