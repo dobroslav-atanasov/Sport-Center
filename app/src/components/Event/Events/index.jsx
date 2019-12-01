2d2a0a1aacd6895a42d905f40ab1158489165e7c
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-12-01 21:19:59 +0200

    Implemented search in Events component.

--- app/src/components/Event/Events/index.jsx
+++ app/src/components/Event/Events/index.jsx
@@ -27,11 +27,36 @@ class Events extends React.Component {
             });
     };
 
+    searchEvents = (event) => {
+        event.preventDefault();
+        const { events } = this.state;
+        const search = event.target.value.toLowerCase();
+        if (search === '') {
+            eventService.getAllEvents()
+                .then(events => {
+                    this.setState({ events: events });
+                });
+        } else {
+            const result = events.filter(x => x.name.toLowerCase().includes(search));
+            this.setState({ events: result });
+        }
+    };
+
     render() {
         const { events } = this.state;
         return (
             <div className="container">
-                <br />
+                <div className="row" style={{ marginBottom: 20, marginTop: 30 }}>
+                    <div className="col-md-4 offset-md-4">
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><i class="fa fa-search"></i></span>
+                            </div>
+                            <input className="form-control mr-sm-2" type="search" name="search" onChange={this.searchEvents} placeholder="Search" />
+                        </div>
+                    </div>
+                </div>
+
                 <table className="table table-bordered table-striped">
                     <thead className="thead-dark">
                         <tr>
