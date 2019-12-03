259db7af2f502a3abb0d993b41b53be37227da5f
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-12-03 08:16:26 +0200

    Installed package react-loading. Modified component Event.

--- app/src/components/Event/Event/index.jsx
+++ app/src/components/Event/Event/index.jsx
@@ -1,44 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import EventInfo from '../EventInfo';
 import eventService from '../../../services/eventService';
+import ReactLoading from 'react-loading';
 
 class Event extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: this.props.match.params.id,
-            event: {}
+            id: this.props.match.params.id
         };
     };
 
-    componentWillMount = () => {
+    componentDidMount = () => {
         eventService.getEvent(this.state.id)
             .then(event => {
-                console.log(event);
-                this.setState({ event: event });
+                this.setState({ event });
             });
     };
 
     render() {
         const { event } = this.state;
         return (
-            <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
-                <div className="row">
-                    <div className="col-md-4">
-                        <EventInfo id={event.id}
-                            name={event.name}
-                            location={event.location}
-                            description={event.description}
-                            imageUrl={event.town.imageUrl}
-                            town={event.town.name}
-                            date={event.date}
-                            participants={event.users.length} />
+            <Fragment>
+                {event ?
+                    <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <EventInfo id={event.id}
+                                    name={event.name}
+                                    location={event.location}
+                                    description={event.description}
+                                    imageUrl={event.town.imageUrl}
+                                    town={event.town.name}
+                                    date={event.date}
+                                    participants={event.users.length} />
+                            </div>
+                            <div className="col-md-8">
+                                sdfsd
+                                </div>
+                        </div>
                     </div>
-                    <div className="col-md-8">
-                        sdfsd
+                    :  <div className="container" style={{ marginTop: 30, marginBottom: 50, width: 100, height: 100 }}>
+                        <ReactLoading type="spin" color="#dc3545" />
+                        <p className="text-danger">
+                            Loading...
+                        </p>
                     </div>
-                </div>
-            </div>
+                }
+            </Fragment>
         );
     };
 };
