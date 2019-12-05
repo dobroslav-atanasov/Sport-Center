b2637430ea87b091bcbc7546162f32f4af1f6f2f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-12-05 18:53:55 +0200

    Modified Home component.

--- app/src/components/Home/index.jsx
+++ app/src/components/Home/index.jsx
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import authService from '../../services/authService';
 import eventService from '../../services/eventService';
 import EventCard from '../Event/EventCard';
-import Loading from '../Loading';
 
 class Home extends React.Component {
     constructor(props) {
@@ -23,56 +23,36 @@ class Home extends React.Component {
         return (
             <Fragment>
                 {user === undefined &&
-                    <Fragment>
-                        <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
-                            <div className="row align-items-center">
-                                <div className="col-md">
-                                    <div className="card" style={{ width: 350 }}>
-                                        <img src="Sport-2.jpg" className="card-img-top" alt="sport-2" />
-                                        <div className="card-body">
-                                            <h5 className="card-title">Card title</h5>
-                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md">
-                                    <div className="card" style={{ width: 350 }}>
-                                        <img src="Sport-3.jpg" className="card-img-top" alt="sport-3" />
-                                        <div className="card-body">
-                                            <h5 className="card-title">Card title</h5>
-                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md">
-                                    <div className="card" style={{ width: 350 }}>
-                                        <img src="Sport-1.jpg" className="card-img-top" alt="sport-1" />
-                                        <div className="card-body">
-                                            <h5 className="card-title">Card title</h5>
-                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        </div>
-                                    </div>
-                                </div>
+                    <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
+                        <div className="card" style={{ width: 1000, marginLeft: 55 }}>
+                            <img src="Sport-1.jpg" className="card-img-top" alt="sport-1" />
+                            <div className="card-body">
+                                <h3 className="card-title text-center">Sport Center</h3>
+                                <h5 className="card-text text-center">Great opportunities come to those who make the most of small ones.</h5>
+                            </div>
+                            <div className="text-center" style={{ marginTop: 10, marginBottom: 10 }}>
+                                <Link to="/register">
+                                    <button className="btn btn-danger btn-lg">
+                                        Join
+                                    </button>
+                                </Link>
                             </div>
                         </div>
-                    </Fragment>
+                    </div>
                 }
-                {user && events ?
-                    <Fragment>
-                        <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
-                            <div className="row align-items-center">
-                                {events.map(e => <EventCard id={e._id}
-                                    name={e.name}
-                                    description={e.description}
-                                    location={e.location}
-                                    town={e.town.name}
-                                    imageUrl={e.town.imageUrl}
-                                    date={e.date}
-                                    participants={e.users.length} />)}
-                            </div>
+                {(user && events) &&
+                    <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
+                        <div className="row align-items-center">
+                            {events.map(e => <EventCard id={e._id}
+                                name={e.name}
+                                description={e.description}
+                                location={e.location}
+                                town={e.town.name}
+                                imageUrl={e.town.imageUrl}
+                                date={e.date}
+                                participants={e.users.length} />)}
                         </div>
-                    </Fragment>
-                    : <Loading />
+                    </div>
                 }
             </Fragment>
         );
