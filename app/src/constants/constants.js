06283fa61261f1d4e3d22eb1c5af21145602abbf
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-12-05 10:34:34 +0200

    Added file with constants.

---
+++ app/src/constants/constants.js
@@ -0,0 +1,40 @@
+const constants = {
+    register: {
+        USERNAME_EXIST: 'Username already exist',
+        INVALID_USERNAME: 'Username should be at least 3 characters long and contains only letters and digits!',
+        INVALID_PASSWORD: 'Password should be at least 3 characters long and contains only letters and digits!',
+        PASSWORD_MATCH: 'Passwords don\'t match!',
+        INVALID_FIRST_NAME: 'First name should be more than 2 symbols and contains only letters!',
+        INVALID_LAST_NAME: 'Last name should be more than 2 symbols and contains only letters!',
+        INVALID_EMAIL: 'Invalid email!',
+        INVALID_AGE: 'Age should be between 16 and 100!',
+        USERNAME_REQUIRED: 'Username is required!',
+        PASSWORD_REQUIRED: 'Password is required!',
+        CONFIRM_PASSWORD_REQUIRED: 'Confirm password is required!',
+        FIRST_NAME_REQUIRED: 'First name is required!',
+        LAST_NAME_REQUIRED: 'Last name is required!',
+        EMAIL_REQUIRED: 'Email is required!',
+        AGE_REQUIRED: 'Age is required!',
+        GENDER_REQUIRED: 'Gender is required!',        
+    },
+
+    login: {
+        INVALID_USERNAME_PASSWORD: 'Invalid username or password!'
+    },
+
+    createTown: {
+        TOWN_EXIST: 'Town already exist!',
+        INVALID_TOWN: 'Town should be at least 3 symbols long and contains only letters!',
+        INVALID_COUNTRY: 'Country should be at least 4 symbols long and contains only letters!',
+        INVALID_IMAGE_URL: 'Image url should start with http or https!',
+        TOWN_NAME_REQUIRED: 'Town name is required!',
+        COUNTRY_NAME_REQUIRED: 'Country name is required!',
+        IMAGE_URL_REQUIRED: 'Image url is required!',
+    },
+
+    createEvent: {
+        
+    }
+};
+
+export default constants;
