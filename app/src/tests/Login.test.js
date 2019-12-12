e3560b6edbe090e79c342afbd8ed5785a37adc20
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-12-12 20:24:09 +0200

    Modified unit tests.

--- app/src/tests/Login.test.js
+++ app/src/tests/Login.test.js
@@ -1,52 +1,32 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Login from '../components/User/Login';
-import renderer from 'react-test-renderer';
-
-jest.mock('react-router-dom', () => ({
-    Link: "Link"
-}));
 
 describe('Login component test', () => {
-    test('Should render component', () => {
-        const component = renderer.create(<Login />);
-        const tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+    const wrapper = shallow(<Login />);
 
-    test('Should have button', () => {
-        const component = renderer.create(<Login />);
-        const root = component.root;
-        const result = root.findByType('button').children.length;
-        expect(result).toEqual(1);
-    });
-
-    test('Should have button with text Log In', () => {
-        const component = renderer.create(<Login />);
-        const root = component.root;
-        const result = root.findByType('button').children[0] === 'Log In';
+    it('Should have input for username', () => {
+        const result = wrapper.find('input#username').length === 1;
         expect(result).toEqual(true);
     });
 
-    test('Should have 2 input fields', () => {
-        const component = renderer.create(<Login />);
-        const root = component.root;
-        const result = root.findAllByType('input').length === 2;
+    it('Username input should have empty state', () => {
+        const result = wrapper.state('username') === '';
         expect(result).toEqual(true);
     });
 
-    test('Should set state of username', () => {
-        const component = renderer.create(<Login />);
-        const root = component.root;
-        const instance = root.instance;
-        instance.setState({username: 'Dobri'})
-        expect(instance.state.username === 'Dobri').toEqual(true);
+    it('Should have input for password', () => {
+        const result = wrapper.find('input#password').length === 1;
+        expect(result).toEqual(true);
     });
-    
-    test('Should set state of password', () => {
-        const component = renderer.create(<Login />);
-        const root = component.root;
-        const instance = root.instance;
-        instance.setState({password: 'Pass'})
-        expect(instance.state.password === 'Pass').toEqual(true);
+
+    it('Password input should have empty state', () => {
+        const result = wrapper.state('password') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Should have a button', () => {
+        const result = wrapper.find('button').text() === 'Log In';
+        expect(result).toEqual(true);
     });
 });
--- app/src/tests/Register.test.js
+++ app/src/tests/Register.test.js
@@ -1,177 +1,153 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Register from '../components/User/Register';
-import renderer from 'react-test-renderer';
-
-jest.mock('react-router-dom', () => ({
-    Link: "Link"
-}));
+import validationService from '../services/validationService';
 
 describe('Register component test', () => {
-    // test('Should render component', () => {
-    //     const component = renderer.create(<Register />);
-    //     const tree = component.toJSON();
-    //     expect(tree).toMatchSnapshot();
-    // });
+    const wrapper = shallow(<Register />);
 
-    test('Should have button', () => {
-        const component = renderer.create(<Register />);
-        const root = component.root;
-        const result = root.findByType('button').children.length;
-        expect(result).toEqual(1);
+    it('Should have input for username', () => {
+        const result = wrapper.find('input#username').length === 1;
+        expect(result).toEqual(true);
     });
 
-    // test('Should have button with text Log In', () => {
-    //     const component = renderer.create(<Login />);
-    //     const root = component.root;
-    //     const result = root.findByType('button').children[0] === 'Log In';
-    //     expect(result).toEqual(true);
-    // });
+    it('Should have input for password', () => {
+        const result = wrapper.find('input#password').length === 1;
+        expect(result).toEqual(true);
+    });
 
-    // test('Should have 2 input fields', () => {
-    //     const component = renderer.create(<Login />);
-    //     const root = component.root;
-    //     const result = root.findAllByType('input').length === 2;
-    //     expect(result).toEqual(true);
-    // });
+    it('Should have input for confirm password', () => {
+        const result = wrapper.find('input#confirmPassword').length === 1;
+        expect(result).toEqual(true);
+    });
 
-    // test('Should set state of username', () => {
-    //     const component = renderer.create(<Login />);
-    //     const root = component.root;
-    //     const instance = root.instance;
-    //     instance.setState({username: 'Dobri'})
-    //     expect(instance.state.username === 'Dobri').toEqual(true);
-    // });
-    
-    // test('Should set state of password', () => {
-    //     const component = renderer.create(<Login />);
-    //     const root = component.root;
-    //     const instance = root.instance;
-    //     instance.setState({password: 'Pass'})
-    //     expect(instance.state.password === 'Pass').toEqual(true);
-    // });
+    it('Should have input for email', () => {
+        const result = wrapper.find('input#email').length === 1;
+        expect(result).toEqual(true);
+    });
+
+    it('Should have input for first name', () => {
+        const result = wrapper.find('input#firstName').length === 1;
+        expect(result).toEqual(true);
+    });
+
+    it('Should have input for last name', () => {
+        const result = wrapper.find('input#lastName').length === 1;
+        expect(result).toEqual(true);
+    });
+
+    it('Should have input for age', () => {
+        const result = wrapper.find('input#age').length === 1;
+        expect(result).toEqual(true);
+    });
+
+    it('Username input should have empty state', () => {
+        const result = wrapper.state('username') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Password input should have empty state', () => {
+        const result = wrapper.state('password') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Confirm password input should have empty state', () => {
+        const result = wrapper.state('confirmPassword') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Email input should have empty state', () => {
+        const result = wrapper.state('email') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('First name input should have empty state', () => {
+        const result = wrapper.state('firstName') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Last name input should have empty state', () => {
+        const result = wrapper.state('lastName') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Age input should have empty state', () => {
+        const result = wrapper.state('age') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Gender input should have empty state', () => {
+        const result = wrapper.state('gender') === '';
+        expect(result).toEqual(true);
+    });
+
+    it('Should have a button', () => {
+        const result = wrapper.find('button').text() === 'Create Account';
+        expect(result).toEqual(true);
+    });
+
+    it('Username validation should return correct result', () => {
+        const result = validationService.usernameValidation('Dobri');
+        expect(result).toEqual(true);
+    });
+
+    it('Username validation should return incorrect result', () => {
+        const result = validationService.usernameValidation('as#$');
+        expect(result).toEqual(false);
+    });
+
+    it('Password validation should return correct result', () => {
+        const result = validationService.passwordValidation('123');
+        expect(result).toEqual(true);
+    });
+
+    it('Password validation should return incorrect result', () => {
+        const result = validationService.passwordValidation('1!');
+        expect(result).toEqual(false);
+    });
+
+    it('Email validation should return correct result', () => {
+        const result = validationService.registerEmailValidation('test@test.com');
+        expect(result).toEqual(true);
+    });
+
+    it('Email validation should return incorrect result', () => {
+        const result = validationService.registerEmailValidation('test@test.bg');
+        expect(result).toEqual(false);
+    });
+
+    it('First name and last name validation should return correct result', () => {
+        const result = validationService.userNamesValidation('Dobroslav');
+        expect(result).toEqual(true);
+    });
+
+    it('First name and last name validation should return incorrect result', () => {
+        const result = validationService.userNamesValidation('d');
+        expect(result).toEqual(false);
+    });
+
+    it('Age validation should return correct result', () => {
+        const result = validationService.registerAgeValidation(18);
+        expect(result).toEqual(true);
+    });
+
+    it('Age validation should return incorrect result', () => {
+        const result = validationService.registerAgeValidation(12);
+        expect(result).toEqual(false);
+    });
+
+    it('Age validation should return incorrect result', () => {
+        const result = validationService.registerAgeValidation(102);
+        expect(result).toEqual(false);
+    });
+
+    it('Match validation should return correct result', () => {
+        const result = validationService.passwordMatchValidation('pass', 'pass');
+        expect(result).toEqual(true);
+    });
+
+    it('Match validation should return incorrect result', () => {
+        const result = validationService.passwordMatchValidation('pass', 'pas');
+        expect(result).toEqual(false);
+    });
 });
-
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import Register from '../components/User/Register';
-// import validationService from '../services/validationService';
-
-// describe('Register component test', () => {
-//     const wrapper = shallow(<Register />);
-
-//     it('Should have input for username', () => {
-//         expect(wrapper.find('input#username').length).toEqual(1);
-//     });
-
-//     it('Should have input for password', () => {
-//         expect(wrapper.find('input#password').length).toEqual(1);
-//     });
-
-//     it('Should have input for confirm password', () => {
-//         expect(wrapper.find('input#confirmPassword').length).toEqual(1);
-//     });
-
-//     it('Should have input for email', () => {
-//         expect(wrapper.find('input#email').length).toEqual(1);
-//     });
-
-//     it('Should have input for first name', () => {
-//         expect(wrapper.find('input#firstName').length).toEqual(1);
-//     });
-
-//     it('Should have input for last name', () => {
-//         expect(wrapper.find('input#lastName').length).toEqual(1);
-//     });
-
-//     it('Should have input for age', () => {
-//         expect(wrapper.find('input#age').length).toEqual(1);
-//     });
-
-//     it('Username input should have empty state', () => {
-//         expect(wrapper.state('username')).toEqual('');
-//     });
-
-//     it('Password input should have empty state', () => {
-//         expect(wrapper.state('password')).toEqual('');
-//     });
-
-//     it('Confirm password input should have empty state', () => {
-//         expect(wrapper.state('confirmPassword')).toEqual('');
-//     });
-
-//     it('Email input should have empty state', () => {
-//         expect(wrapper.state('email')).toEqual('');
-//     });
-
-//     it('First name input should have empty state', () => {
-//         expect(wrapper.state('firstName')).toEqual('');
-//     });
-
-//     it('Last name input should have empty state', () => {
-//         expect(wrapper.state('lastName')).toEqual('');
-//     });
-
-//     it('Age input should have empty state', () => {
-//         expect(wrapper.state('age')).toEqual('');
-//     });
-
-//     it('Gender input should have empty state', () => {
-//         expect(wrapper.state('gender')).toEqual('');
-//     });
-
-//     it('Should have a button', () => {
-//         expect(wrapper.find('button').text()).toEqual('Create Account');
-//     });
-
-//     it('Username validation should return correct result', () => {
-//         expect(validationService.usernameValidation('Dobri')).toEqual(true);
-//     });
-
-//     it('Username validation should return incorrect result', () => {
-//         expect(validationService.usernameValidation('as#$')).toEqual(false);
-//     });
-
-//     it('Password validation should return correct result', () => {
-//         expect(validationService.passwordValidation('123')).toEqual(true);
-//     });
-
-//     it('Password validation should return incorrect result', () => {
-//         expect(validationService.passwordValidation('1!')).toEqual(false);
-//     });
-
-//     it('Email validation should return correct result', () => {
-//         expect(validationService.registerEmailValidation('test@test.com')).toEqual(true);
-//     });
-    
-//     it('Email validation should return incorrect result', () => {
-//         expect(validationService.registerEmailValidation('test@test.bg')).toEqual(false);
-//     });
-
-//     it('First name and last name validation should return correct result', () => {
-//         expect(validationService.userNamesValidation('Dobroslav')).toEqual(true);
-//     });
-    
-//     it('First name and last name validation should return incorrect result', () => {
-//         expect(validationService.userNamesValidation('d')).toEqual(false);
-//     });
-
-//     it('Age validation should return correct result', () => {
-//         expect(validationService.registerAgeValidation(20)).toEqual(true);
-//     });
-    
-//     it('Age validation should return incorrect result', () => {
-//         expect(validationService.userNamesValidation(12)).toEqual(false);
-//     });
-
-//     it('Age validation should return incorrect result', () => {
-//         expect(validationService.userNamesValidation(102)).toEqual(false);
-//     });
-
-//     it('Match validation should return correct result', () => {
-//         expect(validationService.passwordMatchValidation('pass', 'pass')).toEqual(true);
-//     });
-    
-//     it('Match validation should return incorrect result', () => {
-//         expect(validationService.passwordMatchValidation('pass', 'pas')).toEqual(false);
-//     });
-// });
