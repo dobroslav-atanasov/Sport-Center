88f45d4aadce552869cc3134019ca21675c61ecc
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-12-14 19:16:59 +0200

    Refactoring components Standings and Stats. Used hooks.

--- app/src/components/Stats/Stats/index.jsx
+++ app/src/components/Stats/Stats/index.jsx
@@ -1,44 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import PersonalInfo from '../PersonalInfo';
 import userService from '../../../services/userService';
 import authService from '../../../services/authService';
 import Loading from '../../Loading';
 import PersonalStats from '../PersonalStats';
 
-class Stats extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    };
+const Stats = () => {
+    const [user, setUser] = useState(undefined);
 
-    componentDidMount = () => {
-        const userId = authService.getUserInfo().id;        
+    useEffect(() => {
+        const userId = authService.getUserInfo().id;
         userService.getUser(userId)
             .then(user => {
-                this.setState({ user: user });
+                setUser(user);
             });
-    };
+    });
 
-    render() {
-        const { user } = this.state;
-        return (
-            <Fragment>
-                {user ?
-                    <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
-                        <div className="row">
-                            <div className="col-md-5">
-                                <PersonalInfo user={user} />
-                            </div>
-                            <div className="col-md-7">
-                                <PersonalStats user={user}/>
-                            </div>
+    return (
+        <Fragment>
+            {user ?
+                <div className="container" style={{ marginTop: 30, marginBottom: 50 }}>
+                    <div className="row">
+                        <div className="col-md-5">
+                            <PersonalInfo user={user} />
                         </div>
-                    </div >
-                    : <Loading />
-                }
-            </Fragment>
-        );
-    };
+                        <div className="col-md-7">
+                            <PersonalStats user={user} />
+                        </div>
+                    </div>
+                </div >
+                : <Loading />
+            }
+        </Fragment>
+    );
 };
 
 export default Stats;
