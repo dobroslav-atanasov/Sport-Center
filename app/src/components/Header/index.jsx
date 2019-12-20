f51a44bf0f4bbef2cf3422a1683548524b1e9ae5
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-12-20 20:31:08 +0200

    Updated component Header.

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -14,10 +14,10 @@ const Header = () => {
                             <Link to="/add-town" className="nav-link"><i className="fa fa-building"></i> Add Town</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/users" className="nav-link"><i class="fa fa-users"></i> All Users</Link>
+                            <Link to="/users" className="nav-link"><i className="fa fa-users"></i> All Users</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/super-admin-events" className="nav-link"><i class="fa fa-calendar"></i> All Events</Link>
+                            <Link to="/super-admin-events" className="nav-link"><i className="fa fa-calendar"></i> All Events</Link>
                         </li>
                     </ul>
                 }
@@ -27,45 +27,45 @@ const Header = () => {
                             <Link to="/create-event" className="nav-link"><i className="fa fa-etsy"></i> Create Event</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/my-created-events" className="nav-link"><i class="fa fa-calendar"></i> My Created Events</Link>
+                            <Link to="/my-created-events" className="nav-link"><i className="fa fa-calendar"></i> My Created Events</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/my-events" className="nav-link"><i class="fa fa-calendar"></i> My Events</Link>
+                            <Link to="/my-events" className="nav-link"><i className="fa fa-calendar"></i> My Events</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/my-results" className="nav-link"><i class="fa fa-address-book"></i> My Results</Link>
+                            <Link to="/my-results" className="nav-link"><i className="fa fa-address-book"></i> My Results</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/stats" className="nav-link"><i class="fa fa-signal"></i> Stats</Link>
+                            <Link to="/stats" className="nav-link"><i className="fa fa-signal"></i> Stats</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/standings" className="nav-link"><i class="fa fa-list-ol"></i> Standings</Link>
+                            <Link to="/standings" className="nav-link"><i className="fa fa-list-ol"></i> Standings</Link>
                         </li>
                     </ul>
                 }
                 {(user !== undefined && user.role === 'User') &&
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <Link to="/my-events" className="nav-link"><i class="fa fa-calendar"></i> My Events</Link>
+                            <Link to="/my-events" className="nav-link"><i className="fa fa-calendar"></i> My Events</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/my-results" className="nav-link"><i class="fa fa-address-book"></i> My Results</Link>
+                            <Link to="/my-results" className="nav-link"><i className="fa fa-address-book"></i> My Results</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/stats" className="nav-link"><i class="fa fa-signal"></i> Stats</Link>
+                            <Link to="/stats" className="nav-link"><i className="fa fa-signal"></i> Stats</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/standings" className="nav-link"><i class="fa fa-list-ol"></i> Standings</Link>
+                            <Link to="/standings" className="nav-link"><i className="fa fa-list-ol"></i> Standings</Link>
                         </li>
                     </ul>
                 }
                 {user !== undefined &&
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item active">
-                            <Link to="/#" className="nav-link"><i class="fa fa-user"></i> Hello{user.role === 'Admin' && ' Admin'} {user.username}!</Link>
+                            <Link to="/#" className="nav-link"><i className="fa fa-user"></i> Hello{user.role === 'Admin' && ' Admin'} {user.username}!</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link to="/logout" className="nav-link"><i class="fa fa-user-times"></i> Logout</Link>
+                            <Link to="/logout" className="nav-link"><i className="fa fa-user-times"></i> Logout</Link>
                         </li>
                     </ul>
                 }
@@ -73,15 +73,15 @@ const Header = () => {
                     <Fragment>
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <Link to="/about" className="nav-link"><i class="fa fa-info"></i> About Us</Link>
+                                <Link to="/about" className="nav-link"><i className="fa fa-info"></i> About Us</Link>
                             </li>
                         </ul>
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item active">
-                                <Link to="/login" className="nav-link"><i class="fa fa-user"></i> Login</Link>
+                                <Link to="/login" className="nav-link"><i className="fa fa-user"></i> Login</Link>
                             </li>
                             <li className="nav-item active">
-                                <Link to="/register" className="nav-link"><i class="fa fa-user-plus"></i> Register</Link>
+                                <Link to="/register" className="nav-link"><i className="fa fa-user-plus"></i> Register</Link>
                             </li>
                         </ul>
                     </Fragment>
